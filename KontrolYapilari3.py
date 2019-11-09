5c76ee23e7d2ea2c0117b353f6c06d1729a99ed7
İbrahim EDİZ <KURS>
2019-11-09 10:13:46 +0200

    09112019

---
+++ KontrolYapilari3.py
@@ -0,0 +1,45 @@
+# for i in range(0,20):
+#     print(i)
+
+
+# for i in range(0,50,2):
+#     print(i)
+
+# for i in range(0,50):
+#     if i % 2 == 0:
+#         print(i)
+
+# sayi = 374
+# adim=0
+# print(sayi%10*10**adim)
+# sayi //= 10
+# adim = 1
+# print(sayi%10*10**adim)
+# sayi //= 10
+# adim = 2
+# print(sayi%10*10**adim)
+# sayi //= 10
+
+# sayi = input("Sayıyı Giriniz")
+# basamak = len(sayi)
+# sayi = int(sayi)
+# for i in range(0,basamak):
+#     print(sayi%10*10**i)
+#     sayi //= 10
+# sayi = int(input("Sayıyı Giriniz"))
+# sonuc = 1
+# for faruk in range(0,sayi):
+#     sonuc *= faruk + 1
+# print("sonuc",sonuc)
+
+
+# sayi = int(input("Sayıyı Giriniz"))
+# sonuc = 1
+# for faruk in range(1,sayi+1):
+#     sonuc *= faruk 
+# print("sonuc",sonuc)
+
+
+liste = ["Hava","Kapalı","Ama","Akalım","Arabaya"]
+for item in liste:
+    print(item)
